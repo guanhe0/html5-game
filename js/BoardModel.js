508f0cec348fe07a922d45e28b90664866cc5f36
guanhe <307784732@qq.com>
2016-12-09 00:04:51 +0800

    commit for BoardModel.js

--- js/BoardModel.js
+++ js/BoardModel.js
@@ -1,27 +1,29 @@
+var cBoardModel = {};
+cBoardModel.EMPTY = 0;
+cBoardModel.RED = 1;
+cBoardModel.GREEN = 2;
+cBoardModel.ILLEGAL_TURN = -1;
+
 function BoardModel(cols,rows){
 	this._cols = cols || 7;
 	this._rows = rows || 6;
 	this._data = [];
-	this._currentPlayer = BoardModel.RED;
+	this._currentPlayer = cBoardModel.RED;
 	this._totalTokens = 0;
 	this.reset();
 }
 _p = BoardModel.prototype;
-BoardModel.EMPTY = 0;
-BoardModel.RED = 1;
-BoardModel.GREEN = 2;
-BoardModel.ILLEGAL_TURN = -1;
 
-_p.reset = function(){
+_p.reset = function(){//初始化棋盘数据
 	this._data = [];
 	for(var i = 0; i < this._rows; i++){
 		this._data[i] = [];
 		for(var j = 0; j < this._cols; j++){
-			this._data[i][j] = BoardModel.EMPTY;
+			this._data[i][j] = cBoardModel.EMPTY;
 		}
 	}
 	
-	this._currentPlayer = BoardModel.RED;
+	this._currentPlayer = cBoardModel.RED;
 	this._totalTokens = 0;
 }
 _p.getPiece = function(col,row){
@@ -40,7 +42,7 @@ _p.makeTurn = function(column){
 	//检查列是否有效
 	if(column < 0 | column > this._cols){
 		return{
-			status:BoardModel.ILLEGAL_TURN
+			status:cBoardModel.ILLEGAL_TURN
 		}
 	}
 	
@@ -50,7 +52,7 @@ _p.makeTurn = function(column){
 	var row = this._getEmptyRow(column);
 	if(row == -1){
 		return{
-			status:BoardModel.ILLEGAL_TURN
+			status:cBoardModel.ILLEGAL_TURN
 		}
 	}
 	
@@ -68,7 +70,7 @@ _p.makeTurn = function(column){
 		piece:piece
 	}
 };
-_p._getEmptyRow = function(column){
+_p._getEmptyRow = function(column){//获取指定列的空行，如果有则返回行号;
 	for(var i = this._rows - 1;i >= 0; i--){
 		if(!this.getPiece(column,i)){
 			return i;
@@ -76,18 +78,18 @@ _p._getEmptyRow = function(column){
 	}
 	return -1;
 }
-_p._toggleCurrentPlayer = function(){
-	if(this._currentPlayer == BoardModel.RED)
-		this._currentPlayer = BoardModel.GREEN;
+_p._toggleCurrentPlayer = function(){//交换玩家，如果当前玩家是红，则轮到绿;如果当前玩家是绿，则轮到红;
+	if(this._currentPlayer == cBoardModel.RED)
+		this._currentPlayer = cBoardModel.GREEN;
 	else
-		this._currentPlayer = BoardModel.RED;
+		this._currentPlayer = cBoardModel.RED;
 }
-BoardModel.NONE = 0;//no win or fail
-BoardModel.WIN = 1;//current player win
-BoardModel.DRAW = 2;//no win and no fail
-BoardModel.ILLEGAL_TURN = 3;//上次试图放置球的操作无效
+cBoardModel.NONE = 0;//no win or fail
+cBoardModel.WIN = 1;//current player win
+cBoardModel.DRAW = 2;//no win and no fail
+cBoardModel.ILLEGAL_TURN = 3;//上次试图放置球的操作无效
 
-_p._checkWinDirection = function(column,row,deltaX,deltaY){
+_p._checkWinDirection = function(column,row,deltaX,deltaY){//从column,row发射(deltaX,deltaY)出去计算相同颜色的球有多少
 	var pieceColor = this.getPiece(column,row);
 	var tokenCounter = 0;
 	var c = column + deltaX;
@@ -99,9 +101,9 @@ _p._checkWinDirection = function(column,row,deltaX,deltaY){
 	}
 	return tokenCounter;
 };
-_p._getGameState = function(column,row){
+_p._getGameState = function(column,row){//获取游戏状态，平局或者还没有结果，每下一个棋判断一次当前棋导致的状态
 	if(this._totalTokens == Game.BOARD_WINDT*Game.BOARD_HEIGHT)
-		return BoardModel.DRAW;
+		return cBoardModel.DRAW;
 	
 	for(var deltaX = -1;deltaX < 2; deltaX++){
 		for(var deltaY = -1;deltaY < 2; deltaY++){
@@ -111,9 +113,9 @@ _p._getGameState = function(column,row){
 			+ this._checkWinDirection(column,row,-deltaX,-deltaY) + 1;
 			
 			if(count >= 4){
-				return BoardModel.WIN;
+				return cBoardModel.WIN;
 			}
 		}
 	}
-	return BoardModel.NONE;
+	return cBoardModel.NONE;
 }
